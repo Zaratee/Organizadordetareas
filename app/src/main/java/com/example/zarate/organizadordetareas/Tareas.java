0286caa88ece28a3ab2ad3851573cd3b44220509
Zaratee <39096560+Zaratee@users.noreply.github.com>
2018-06-18 01:22:47 -0500

    Initial commit

---
+++ app/src/main/java/com/example/zarate/organizadordetareas/Tareas.java
@@ -0,0 +1,13 @@
+package com.example.zarate.organizadordetareas;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Tareas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tareas);
+    }
+}
